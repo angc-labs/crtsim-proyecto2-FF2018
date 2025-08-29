8b1522a931b5649183fa5eecde2273d2420d00f7
angcoder-c <angcoder.c@gmail.com>
2025-08-29 12:10:38 -0600

    funciones basicas de canvas

--- src/main.js
+++ src/main.js
@@ -1,1 +1,26 @@
-console.log("hello")
+const canvas = document.getElementById('vista-frontal')
+const botonBorrar = document.getElementById('boton')
+
+botonBorrar.onclick=clear
+const demoCanvas = canvas.getContext('2d');
+
+window.onload = function() {
+    // variables para el circulo
+    const r = 50
+    const x = 200
+    const y=50
+    demoCanvas.beginPath();
+    demoCanvas.moveTo(0, 0); 
+    demoCanvas.lineTo(400, 0);
+    demoCanvas.stroke();
+    demoCanvas.fillStyle = 'blue'
+    demoCanvas.moveTo(x+r,y)
+    demoCanvas.arc(x, y, r, 0, 2 * Math.PI);
+    demoCanvas.fill()
+    demoCanvas.stroke();
+    demoCanvas.closePath()
+}
+
+function clear (){
+    demoCanvas.clearRect(0, 0, canvas.width, canvas.height);
+}
